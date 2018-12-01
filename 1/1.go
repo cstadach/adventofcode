1fe8c91c83d65735c02719f3199e66dfac9f4a72
Christian Stadach <c.stadach@emetriq.com>
2018-12-01 10:02:58 +0100

    added first solution

--- 1/1.go
+++ 1/1.go
@@ -1,1 +1,29 @@
-package _
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main () {
+	file, err := os.Open("./input")
+	check(err)
+	defer file.Close()
+
+	frequency := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan(){
+		number, err := strconv.Atoi(scanner.Text())
+		check(err)
+		frequency += number
+	}
+	fmt.Print(frequency)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
