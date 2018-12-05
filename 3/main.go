1cde42ec4fa83b35d8c57c4633982f8eabb19da2
Christian Stadach <c.stadach@emetriq.com>
2018-12-05 08:53:51 +0100

    solution for 312_1

--- 3/main.go
+++ 3/main.go
@@ -1,5 +1,82 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func main() {
-	
+	inputs := LoadInputFile()
+	fmt.Println(countOverlapping(buildMatrix(inputs)))
+}
+
+type field struct {
+	id string
+	xCoord int
+	yCoord int
+	width int
+	height int
+}
+
+func countOverlapping(matrix map[string]int) int {
+	result := 0
+	for _, value := range matrix {
+        if value > 1 {result++}
+	}
+	return result
+}
+
+func buildMatrix(inputs []string) map[string]int {
+	matrix := map[string]int{}
+	for _, input := range inputs {
+		field := newField(input)
+		for x := field.xCoord; x < field.xCoord + field.width; x++ {
+			for y := field.yCoord; y < field.yCoord + field.height; y++ {
+				matrix[fmt.Sprintf("%dx%d",x,y)] += 1
+			}
+		}
+	}
+	return matrix
+}
+
+//#123 @ 3,2: 5x4
+func newField(input string) field {
+	inputFields := strings.Split(input," ")
+	coordinates := strings.Split(inputFields[2][:len(inputFields[2])-1],",")
+	xCoord, _ := strconv.Atoi(coordinates[0])
+	yCoord, _ := strconv.Atoi(coordinates[1])
+	dimensions := strings.Split(inputFields[3], "x")
+	width, _ := strconv.Atoi(dimensions[0])
+	height, _ := strconv.Atoi(dimensions[1])
+
+	return field{
+		id: inputFields[0],
+		xCoord: xCoord,
+		yCoord: yCoord,
+		width: width,
+		height: height,
+	}
+}
+
+func LoadInputFile() []string {
+	file, err := os.Open("input")
+	Check(err)
+	defer file.Close()
+
+	var inputs []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		input := scanner.Text()
+		inputs = append(inputs, input)
+	}
+	return inputs
+}
+
+func Check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
--- 3/main_test.go
+++ 3/main_test.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestOverlappingFields(t *testing.T) {
+	coordinates := []string{
+		"#1 @ 1,3: 4x4",
+		"#2 @ 3,1: 4x4",
+		"#3 @ 5,5: 2x2",}
+	result := buildMatrix(coordinates)
+	if !(assert.Condition(t, func() bool {return result["3x3"] > 1}) &&
+		assert.Condition(t, func() bool {return result["4x3"] > 1}) &&
+		assert.Condition(t, func() bool {return result["3x4"] > 1}) &&
+		assert.Condition(t, func() bool {return result["4x4"] > 1})) {
+		t.Error( "Expected overlapping fields did not match" )
+	}
+}
+
+func TestOverlappingFieldsCount(t *testing.T) {
+    coordinates := []string{
+		"#1 @ 1,3: 4x4",
+		"#2 @ 3,1: 4x4",
+		"#3 @ 5,5: 2x2",}
+    result := countOverlapping(buildMatrix(coordinates))
+    if !(assert.Equal(t, 4, result)) {
+		t.Error( "Expected overlapping fields did not match" )
+	}
+}
