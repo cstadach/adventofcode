a6726accce48473e9cc0f08f5e396176fbccdda9
Christian Stadach <c.stadach@emetriq.com>
2018-12-06 09:06:24 +0100

    3rd day second task

--- 3/main.go
+++ 3/main.go
@@ -11,31 +11,51 @@ import (
 func main() {
 	inputs := LoadInputFile()
 	fmt.Println(countOverlapping(buildMatrix(inputs)))
+	fmt.Println(findLonelyField(inputs))
 }
 
 type field struct {
-	id string
+	id     string
 	xCoord int
 	yCoord int
-	width int
+	width  int
 	height int
 }
 
 func countOverlapping(matrix map[string]int) int {
 	result := 0
 	for _, value := range matrix {
-        if value > 1 {result++}
+		if value > 1 {
+			result++
+		}
 	}
 	return result
+}
+
+func findLonelyField(inputs []string) string {
+	matrix := buildMatrix(inputs)
+	for _, input := range inputs {
+		field := newField(input)
+		result := true
+		for x := field.xCoord; x < field.xCoord+field.width; x++ {
+			for y := field.yCoord; y < field.yCoord+field.height; y++ {
+				result = result && matrix[fmt.Sprintf("%dx%d", x, y)] == 1
+			}
+		}
+		if result {
+			return field.id
+		}
+	}
+	return ""
 }
 
 func buildMatrix(inputs []string) map[string]int {
 	matrix := map[string]int{}
 	for _, input := range inputs {
 		field := newField(input)
-		for x := field.xCoord; x < field.xCoord + field.width; x++ {
-			for y := field.yCoord; y < field.yCoord + field.height; y++ {
-				matrix[fmt.Sprintf("%dx%d",x,y)] += 1
+		for x := field.xCoord; x < field.xCoord+field.width; x++ {
+			for y := field.yCoord; y < field.yCoord+field.height; y++ {
+				matrix[fmt.Sprintf("%dx%d", x, y)] += 1
 			}
 		}
 	}
@@ -44,8 +64,8 @@ func buildMatrix(inputs []string) map[string]int {
 
 //#123 @ 3,2: 5x4
 func newField(input string) field {
-	inputFields := strings.Split(input," ")
-	coordinates := strings.Split(inputFields[2][:len(inputFields[2])-1],",")
+	inputFields := strings.Split(input, " ")
+	coordinates := strings.Split(inputFields[2][:len(inputFields[2])-1], ",")
 	xCoord, _ := strconv.Atoi(coordinates[0])
 	yCoord, _ := strconv.Atoi(coordinates[1])
 	dimensions := strings.Split(inputFields[3], "x")
@@ -53,10 +73,10 @@ func newField(input string) field {
 	height, _ := strconv.Atoi(dimensions[1])
 
 	return field{
-		id: inputFields[0],
+		id:     inputFields[0],
 		xCoord: xCoord,
 		yCoord: yCoord,
-		width: width,
+		width:  width,
 		height: height,
 	}
 }
